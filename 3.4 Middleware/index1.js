c25c0bb2d98ca9959f6b424d2e9be904b4411593
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-10-08 00:13:21 +0530

    Custom Middleware and Band Generater Project

--- 3.4 Middleware/index1.js
+++ 3.4 Middleware/index1.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
--- 3.4 Middleware/index2.js
+++ 3.4 Middleware/index2.js
@@ -1,7 +1,8 @@
 import express from "express";
-
 const app = express();
 const port = 3000;
+
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("Hello");
