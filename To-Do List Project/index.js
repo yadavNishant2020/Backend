db5cc9a8218baf38a7edaf086cd74be108e9aed4
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-10-20 00:16:39 +0530

    Features Added - adding the task to the list

--- To-Do List Project/index.js
+++ To-Do List Project/index.js
@@ -4,12 +4,20 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/',(req, res)=>{
     const date = new Date();
     res.render("index.ejs",
     {toDay: date}
     );
 })
+
+app.post('/',(req, res)=>{
+    res.render("index.ejs",
+    {taskToAdd: req.body["task"]});
+})
+
 
 app.get('/work',(req, res)=>{
     res.render("index.ejs",
