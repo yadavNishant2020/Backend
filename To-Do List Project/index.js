3d5af904d51004806d7fe58c1e4215b40a8377d9
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2023-10-20 12:59:40 +0530

    Features Added - Task adding and showing in the list - Strikeoff Tasks

--- To-Do List Project/index.js
+++ To-Do List Project/index.js
@@ -1,31 +1,39 @@
 import express from "express";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/',(req, res)=>{
-    const date = new Date();
-    res.render("index.ejs",
-    {toDay: date}
-    );
-})
+function checkCheckboxStatus() {
+    const checkbox = document.getElementById("myCheckbox");
 
-app.post('/',(req, res)=>{
-    res.render("index.ejs",
-    {taskToAdd: req.body["task"]});
-})
-
-
-app.get('/work',(req, res)=>{
-    res.render("index.ejs",
-    {}
-    );
-})
+    if (checkbox.checked) {
+      console.log("Checkbox is checked");
+    } else {
+      console.log("Checkbox is not checked");
+    }
+  }
+app.get("/", (req, res) => {
 
 
-app.listen(port,() =>{
-    console.log(`Server is running on ${port}`);
-})
+  const date = new Date();
+  res.render("index.ejs", { toDay: date, checkCheckboxStatus: checkCheckboxStatus });
+});
+
+const taskArray = [];
+
+app.post("/", (req, res) => {
+  const newTask = req.body.task; //taking input tasks from user
+  taskArray.push(newTask);
+  res.render("index.ejs", { tasks: taskArray });
+});
+
+app.get("/work", (req, res) => {
+  res.render("index.ejs", {});
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
